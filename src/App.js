0b515afe18cb4d543bc218bf4d7975ee8b67f38b
DylanTHK <dylantanhk@gmail.com>
2024-11-13 22:42:41 +0800

    baseline tictactoe board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React from 'react';
+
+function Square({ input }) {
+  function handleClick() {
+    console.log("clicked");
+  }
+  return (
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {input}
+    </button>
+  )
+}
+  
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square input="1"/>
+        <Square input="2"/>
+        <Square input="3"/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+    </>
+  );
 }
